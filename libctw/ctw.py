557f2f73a933017d0873d0212e8283029e88a03f
Ivo Danihelka <ivo@danihelka.net>
2011-05-15 22:34:09 +0200

    Fixed context creation.

--- libctw/ctw.py
+++ libctw/ctw.py
@@ -96,7 +96,7 @@ class _CtModel:
         """Returns the recent context.
         """
         context = self.history
-        if self.max_depth is not None:
+        if self.max_depth is not None and len(context) > self.max_depth:
             context = context[len(context) - self.max_depth:]
             assert len(context) <= self.max_depth
         return context
